1f6ad62d3f95b797d31b3cae4ce3d2b188593fff
Colin Delahunty <colin99delahunty@gmail.com>
2025-06-13 20:30:56 -0400

    Added fixes

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,56 @@
 use axum::http::StatusCode;
 use axum::{
-    extract,
+    extract::{
+        State, Json
+    },
     routing::post,
     Router,
+    response::{IntoResponse, Response}
 };
-use axum::response::IntoResponse;
 use lambda_http::{run, tracing, Error};
 use std::env::set_var;
 use schemas::documenso::WebhookEvent;
 use stripe::{Event, EventObject, EventType};
+use sqlx::{MySqlPool, query};
 
 pub mod schemas;
 
-async fn documenso(payload: extract::Json<WebhookEvent>) -> impl IntoResponse {
+async fn documenso(payload: Json<WebhookEvent>) -> impl IntoResponse {
     println!("Received documenso webhook event: {:?}", payload);
     StatusCode::OK
 }
 
-async fn stripe(event: extract::Json<Event>) -> impl IntoResponse {
-    if let EventType::CheckoutSessionCompleted = event.type_ {
-        if let EventObject::CheckoutSession(session) = &event.data.object {
-            println!("Оплата завершена, session id = {}", session.id);
-        }
+pub async fn stripe(
+    State(pool): State<MySqlPool>,
+    Json(event): Json<Event>,
+) -> Response {
+    let session = match (&event.type_, &event.data.object) {
+        (EventType::PaymentIntentSucceeded, EventObject::PaymentIntent(s)) => s,
+        _ => return (StatusCode::ACCEPTED, "ignored").into_response(),
+    };
+
+
+    let sale_id = match session.metadata.get("saleId") {
+        Some(id) => id.as_str(),
+        None => return (StatusCode::UNPROCESSABLE_ENTITY, "saleId missing").into_response(),
+    };
+
+    let result = query!(
+        r#"INSERT INTO stripe_payments
+           (sale_id, stripe_payment_intent_id, amount_total)
+           VALUES (?, ?, ?)"#,
+        sale_id,
+        session.id.as_str(),
+        session.amount
+    )
+    .execute(&pool)
+    .await;
+
+    // всегда возвращаем текст
+    match result {
+        Ok(_)  => (StatusCode::CREATED,               "created").into_response(),
+        Err(e) => (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()).into_response(),
     }
-    StatusCode::OK
 }
 
 #[tokio::main]
@@ -35,13 +62,16 @@ async fn main() -> Result<(), Error> {
     // you can remove it if you don't use them.
     // i.e with: `GET /test-stage/todo/id/123` without: `GET /todo/id/123`
     set_var("AWS_LAMBDA_HTTP_IGNORE_STAGE_IN_PATH", "true");
+    let database_url = std::env::var("DATABASE_URL")?;
+    let pool = MySqlPool::connect(&database_url).await?;
 
     // required to enable CloudWatch error logging by the runtime
     tracing::init_default_subscriber();
 
     let app = Router::new()
         .route("/documenso", post(documenso))
-        .route("/stripe", post(stripe));
+        .route("/stripe", post(stripe))
+        .with_state(pool);
 
     run(app).await
 }
