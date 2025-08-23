b8a2439c7d646710111f7d147f6ef3087101f3dc
Dima <dmitryzaika9@gmail.com>
2025-08-23 14:09:58 +0000

    Refactor FaceBookContactForm schema to rename fields for clarity and consistency, aligning identifiers with standard naming conventions for improved data mapping.

--- src/schemas/add_customer.rs
+++ src/schemas/add_customer.rs
@@ -50,33 +50,33 @@ pub struct WordpressContactForm {
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct FaceBookContactForm {
-    #[serde(rename = "1.data.full_name")]
+    #[serde(rename = "name")]
     pub name: String,
 
-    #[serde(rename = "1.data.phone_number")]
+    #[serde(rename = "phone")]
     pub phone: String,
 
-    #[serde(rename = "1.data.would_you_like_us_to_remove_and_dispose_of_your_old_countertops?")]
+    #[serde(rename = "remove")]
     pub remove_and_dispose: Option<String>,
 
-    #[serde(rename = "1.data.email")]
+    #[serde(rename = "email")]
     pub email: Option<String>,
 
-    #[serde(rename = "1.data.city")]
+    #[serde(rename = "city")]
     pub city: Option<String>,
 
-    #[serde(rename = "1.data.zip_code")]
+    #[serde(rename = "zip")]
     pub postal_code: Option<String>,
 
-    #[serde(rename = "1.data.what_other_information_you'd_like_to_share?_(e.g_sqft,_state_etc.)")]
+    #[serde(rename = "share")]
     pub details: Option<String>,
 
-    #[serde(rename = "1.campaignName")]
+    #[serde(rename = "campaign")]
     pub compaign_name: Option<String>,
 
-    #[serde(rename = "1.adsetName")]
+    #[serde(rename = "adsetname")]
     pub adset_name: Option<String>,
 
-    #[serde(rename = "1.adName")]
+    #[serde(rename = "adname")]
     pub ad_name: Option<String>,
 }
