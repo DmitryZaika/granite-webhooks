642c7babe62c3e2747947c8597b8ea24ab97217b
Anna Kuhir <anna.kuhir@gmail.com>
2026-03-23 17:00:21 +0200

    feat: change the super admin page implementation

--- migrations/20260319190530_seed_superadmin_companies.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO superadmin_companies (user_id, company_id)
-SELECT u.id, u.company_id
-FROM users u
-WHERE u.is_superuser = 1
-  AND u.is_deleted = 0
-  AND u.company_id >= 0
-  AND u.id NOT IN (SELECT sc.user_id FROM superadmin_companies sc);
