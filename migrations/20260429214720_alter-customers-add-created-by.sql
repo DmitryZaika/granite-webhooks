5603bbe7ffb9c730e6c62aa0a3262692e7bf1277
Dima <dmitryzaika9@gmail.com>
2026-04-29 23:58:21 +0000

    feat: add migration scripts to enhance customers table with created_by column and create customers_history table

--- migrations/20260429214720_alter-customers-add-created-by.sql
+++ migrations/20260429214720_alter-customers-add-created-by.sql
@@ -1,2 +1,3 @@
--- Add migration script here
-ALTER TABLE customers ADD COLUMN created_by VARCHAR(100) NULL;
+-- Migration to add 'created_by' and 'assigned_by' columns to customers table
+ALTER TABLE customers
+    ADD COLUMN created_by VARCHAR(100) NULL
