fe6a16a9cbb6c8192914630248c41712377459b7
Colin Delahunty <colin99delahunty@gmail.com>
2026-05-31 13:09:07 -0400

    Allow the refactor

--- webhooks/src/google/receive.rs
+++ webhooks/src/google/receive.rs
@@ -1,7 +1,7 @@
 use crate::axum_helpers::guards::RemixBackend;
-use crate::google::maps::get_address_autocomplete;
+use crate::google::maps::get_all_address_autocompletes;
 use crate::google::schemas::{AddressRequest, FinalSuggestion};
-use crate::libs::constants::{NOT_FOUND_RESPONSE, internal_error};
+use crate::libs::constants::internal_error;
 use crate::libs::types::BasicResponse;
 
 use axum::extract;
@@ -11,10 +11,9 @@ use lambda_http::tracing;
 pub async fn address_information(
     _: RemixBackend,
     extract::Json(payload): extract::Json<AddressRequest>,
-) -> Result<Json<FinalSuggestion>, BasicResponse> {
-    match get_address_autocomplete(&payload.query).await {
-        Ok(Some(response)) => Ok(Json(response)),
-        Ok(None) => Err(NOT_FOUND_RESPONSE),
+) -> Result<Json<Vec<FinalSuggestion>>, BasicResponse> {
+    match get_all_address_autocompletes(&payload.query).await {
+        Ok(response) => Ok(Json(response)),
         Err(err) => {
             tracing::error!("Unable to get address autocomplete: {:?}", err);
             Err(internal_error("Unable to get address autocomplete"))
